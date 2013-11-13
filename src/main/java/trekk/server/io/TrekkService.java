69e00c252d0ff93017df92d64712542709eba3af
marsmining <marsmining@gmail.com>
2013-11-13 20:48:08 +0100

    add remaining functionality to the server api

--- src/main/java/trekk/server/io/TrekkService.java
+++ src/main/java/trekk/server/io/TrekkService.java
@@ -1,8 +1,19 @@
 package trekk.server.io;
 
 import trekk.server.model.Customer;
+import trekk.server.model.Navigation;
 
 public interface TrekkService {
 
     Iterable<Customer> findAll();
+
+    Customer create(Customer customer);
+
+    Customer findCustomer(Long id);
+
+    Customer update(Customer customer);
+
+    void delete(Long id);
+
+    Iterable<Navigation> findByCustomerId(Long id);
 }
--- src/main/java/trekk/server/io/TrekkServiceImpl.java
+++ src/main/java/trekk/server/io/TrekkServiceImpl.java
@@ -6,20 +6,51 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import trekk.server.model.Customer;
+import trekk.server.model.Navigation;
 import trekk.server.repo.CustomerRepo;
+import trekk.server.repo.NavigationRepo;
 
 @Named
 public class TrekkServiceImpl implements TrekkService {
 
     private final CustomerRepo customerRepo;
+    private final NavigationRepo navigationRepo;
 
     @Inject
-    public TrekkServiceImpl(final CustomerRepo customerRepo) {
+    public TrekkServiceImpl(final CustomerRepo customerRepo,
+            final NavigationRepo navigationRepo) {
         this.customerRepo = checkNotNull(customerRepo);
+        this.navigationRepo = checkNotNull(navigationRepo);
     }
 
     @Override
     public Iterable<Customer> findAll() {
         return customerRepo.findAll();
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        customer.setId(null);
+        return customerRepo.save(customer);
+    }
+
+    @Override
+    public Customer findCustomer(Long id) {
+        return customerRepo.findOne(id);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepo.delete(id);
+    }
+
+    @Override
+    public Iterable<Navigation> findByCustomerId(Long id) {
+        return navigationRepo.findByCustomerId(id);
     }
 }
